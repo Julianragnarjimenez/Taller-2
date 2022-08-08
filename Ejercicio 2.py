7b147d0a6eb4dfffc5154034f286623b0838baf4
Julianragnarjimenez <110119255+Julianragnarjimenez@users.noreply.github.com>
2022-08-07 19:39:48 -0500

    Add files via upload

---
+++ Ejercicio 2.py
@@ -0,0 +1,4 @@
+capital=float(input("Escribe el capital a a invertir"))
+ganancia= capital*.02
+print("La ganacia obtenida por el capital invertido es: $",ganancia)
+print("La ganancia total ya con el capital invertido es: ",capital+ganancia)
---
+++ Ejercicio 3.py
@@ -0,0 +1,10 @@
+sueldo=float(input("ingresa el sueldo"))
+a= flotat(input("ingresa la venta 1"))
+b= float(input("ingreasa el valor 2"))
+c= float(input("ingresa la venta 3"))
+
+comision= (a+b+c)*.10
+
+print("El sueldo del trabajador es: $",sueldo)
+print("La comision del mes por las 3 ventas es: ",comision)
+print("El sueldo total ya con la comision es: $",sueldo + comision )
