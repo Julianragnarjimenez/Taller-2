dfd095b8148f22428d20d535c214f0ec036aa936
Julianragnarjimenez <110119255+Julianragnarjimenez@users.noreply.github.com>
2022-08-07 19:03:31 -0500

    Add files via upload

---
+++ Ejercicio 1.py
@@ -0,0 +1,8 @@
+# entradas
+edad_uno = int(input("digite edad uno "))
+edad_dos = int(input("digite edad dos "))
+edad_tres = int(input("digite edad tres "))
+# caja negra
+promedio = (edad_uno + edad_dos + edad_tres) / 3
+# salida
+print("promedio: ", promedio)
