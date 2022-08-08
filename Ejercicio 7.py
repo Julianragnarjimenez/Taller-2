599ddd76aa929d95352e3773560875f87df87551
Julianragnarjimenez <110119255+Julianragnarjimenez@users.noreply.github.com>
2022-08-07 22:02:27 -0500

    Add files via upload

---
+++ Ejercicio 7.py
@@ -0,0 +1,7 @@
+metros = float (input ('Ingresa el valor de metros: '))
+pulgadas=metros*39.27
+pies=pulgadas/12
+print ("Valor de pies: " , pies)
+print ("Valor de pulgadas:", pulgadas)
+
+print("promedio matematicas {promediom}, fisica {promediof}, quimica {promedioq},el promedi semestre es:{promedio_final})
