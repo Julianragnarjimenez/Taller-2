0f4f4e216e013e2d12cde4a8773092268c814ce2
Julianragnarjimenez <110119255+Julianragnarjimenez@users.noreply.github.com>
2022-08-07 21:16:24 -0500

    Add files via upload

---
+++ Ejercicio 19.py
@@ -0,0 +1,12 @@
+# entradas
+Capital =(float(input("ingrese numero naranja : ")))
+Tiempo =(float(input("ingrese    : ")))
+Razon =(float(input("ingrese razon : ")))
+# caja negra
+n="naranjas"
+X=48000
+Y=6
+K=42000
+ganancia= X+Y+K*0.75
+# salidas
+print ("Valor de ganancia: " , ganancia)
